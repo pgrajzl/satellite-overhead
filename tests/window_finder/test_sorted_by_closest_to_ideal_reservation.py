d05ecb48f6174f542e584468c11416a051688b9e
nikojpapa <npapa1994@gmail.com>
2022-12-01 15:55:20 -0700

    window finder returns list of overhead satellites on suggestions

--- tests/window_finder/test_sorted_by_closest_to_ideal_reservation.py
+++ tests/window_finder/test_sorted_by_closest_to_ideal_reservation.py
@@ -3,7 +3,7 @@ from typing import List
 
 from satellite_determination.dataclasses.reservation import Reservation
 from satellite_determination.dataclasses.time_window import TimeWindow
-from satellite_determination.window_finder import WindowFinder
+from satellite_determination.window_finder import SuggestedReservation, WindowFinder
 from tests.window_finder.definitions import ARBITRARY_FACILITY
 from tests.window_finder.support.validator_satellites_are_overhead_at_specific_times import \
     ValidatorSatellitesAreOverheadAtSpecificTimes
@@ -12,7 +12,7 @@ from tests.window_finder.support.validator_satellites_are_overhead_at_specific_t
 class TestSortedByClosestToIdealReservation:
     def test(self):
         suggestions = WindowFinder(
-            ideal_reservation=self._reservation,
+            ideal_reservation=self._ideal_reservation,
             satellites=[],
             validator=ValidatorSatellitesAreOverheadAtSpecificTimes(overhead_times=[]),
             start_time_increments=timedelta(days=1),
@@ -21,13 +21,24 @@ class TestSortedByClosestToIdealReservation:
         assert suggestions == self._expected_suggestions
 
     @property
-    def _reservation(self) -> Reservation:
+    def _expected_suggestions(self) -> List[SuggestedReservation]:
+        return [
+            SuggestedReservation(
+                suggested_start_time=start_time,
+                ideal_reservation=self._ideal_reservation,
+                overhead_satellites=[]
+            )
+            for start_time in self._expected_suggestion_start_times
+        ]
+
+    @property
+    def _ideal_reservation(self) -> Reservation:
         return Reservation(
             facility=ARBITRARY_FACILITY,
             time=TimeWindow(begin=datetime(year=2022, month=11, day=20), end=datetime(year=2022, month=11, day=21)))
 
     @property
-    def _expected_suggestions(self) -> List[datetime]:
+    def _expected_suggestion_start_times(self) -> List[datetime]:
         return [
             datetime(year=2022, month=11, day=20),
             datetime(year=2022, month=11, day=21),
--- tests/window_finder/test_sorted_by_least_number_of_satellites.py
+++ tests/window_finder/test_sorted_by_least_number_of_satellites.py
@@ -6,16 +6,20 @@ from satellite_determination.dataclasses.overhead_window import OverheadWindow
 from satellite_determination.dataclasses.reservation import Reservation
 from satellite_determination.dataclasses.satellite import Satellite
 from satellite_determination.dataclasses.time_window import TimeWindow
-from satellite_determination.window_finder import WindowFinder
+from satellite_determination.window_finder import SuggestedReservation, WindowFinder
 from tests.window_finder.definitions import ARBITRARY_FACILITY
 from tests.window_finder.support.validator_satellites_are_overhead_at_specific_times import \
     ValidatorSatellitesAreOverheadAtSpecificTimes
 
 
+_ARBITRARY_FREQUENCY_RANGE = FrequencyRange(high_in_megahertz=2., low_in_megahertz=1.)
+_ARBITRARY_SATELLITE_NUMBER = 'satellite_number'
+
+
 class TestSortedByLeastNumberOfSatellites:
     def test(self):
         suggestions = WindowFinder(
-            ideal_reservation=self._reservation,
+            ideal_reservation=self._ideal_reservation,
             satellites=[window.satellite for window in self._overhead_windows],
             validator=ValidatorSatellitesAreOverheadAtSpecificTimes(
                 overhead_times=[window.overhead_time for window in self._overhead_windows]),
@@ -26,34 +30,58 @@ class TestSortedByLeastNumberOfSatellites:
 
     @property
     def _overhead_windows(self) -> List[OverheadWindow]:
-        arbitrary_frequency_range = FrequencyRange(high_in_megahertz=2., low_in_megahertz=1.)
-        arbitrary_satellite_number = 'satellite_number'
+        return self._two_overhead_windows_on_ideal_reservation + self._one_overhead_window_on_second_closest_reservation
+
+    @property
+    def _expected_suggestions(self) -> List[SuggestedReservation]:
+        ideal_reservation_index = len(self._expected_suggestion_start_times) - 1
+        second_closest_reservation_index = ideal_reservation_index - 1
         return [
-            OverheadWindow(satellite=Satellite(frequency=arbitrary_frequency_range,
-                                               satellite_number=arbitrary_satellite_number,
+            SuggestedReservation(
+                suggested_start_time=start_time,
+                ideal_reservation=self._ideal_reservation,
+                overhead_satellites=self._two_overhead_windows_on_ideal_reservation
+                    if index == ideal_reservation_index
+                    else self._one_overhead_window_on_second_closest_reservation
+                        if index == second_closest_reservation_index
+                        else []
+            )
+            for index, start_time in enumerate(self._expected_suggestion_start_times)
+        ]
+
+    @property
+    def _ideal_reservation(self) -> Reservation:
+        return Reservation(
+            facility=ARBITRARY_FACILITY,
+            time=TimeWindow(begin=datetime(year=2022, month=11, day=20), end=datetime(year=2022, month=11, day=21)))
+
+    @property
+    def _two_overhead_windows_on_ideal_reservation(self) -> List[OverheadWindow]:
+        return [
+            OverheadWindow(satellite=Satellite(frequency=_ARBITRARY_FREQUENCY_RANGE,
+                                               satellite_number=_ARBITRARY_SATELLITE_NUMBER,
                                                name='name1'),
                            overhead_time=TimeWindow(begin=datetime(year=2022, month=11, day=20),
                                                     end=datetime(year=2022, month=11, day=20, hour=1))),
-            OverheadWindow(satellite=Satellite(frequency=arbitrary_frequency_range,
-                                               satellite_number=arbitrary_satellite_number,
+            OverheadWindow(satellite=Satellite(frequency=_ARBITRARY_FREQUENCY_RANGE,
+                                               satellite_number=_ARBITRARY_SATELLITE_NUMBER,
                                                name='name2'),
                            overhead_time=TimeWindow(begin=datetime(year=2022, month=11, day=20),
-                                                    end=datetime(year=2022, month=11, day=20, hour=1))),
-            OverheadWindow(satellite=Satellite(frequency=arbitrary_frequency_range,
-                                               satellite_number=arbitrary_satellite_number,
+                                                    end=datetime(year=2022, month=11, day=20, hour=1)))
+        ]
+
+    @property
+    def _one_overhead_window_on_second_closest_reservation(self) -> List[OverheadWindow]:
+        return [
+            OverheadWindow(satellite=Satellite(frequency=_ARBITRARY_FREQUENCY_RANGE,
+                                               satellite_number=_ARBITRARY_SATELLITE_NUMBER,
                                                name='name3'),
                            overhead_time=TimeWindow(begin=datetime(year=2022, month=11, day=21),
                                                     end=datetime(year=2022, month=11, day=21, hour=1))),
         ]
 
     @property
-    def _reservation(self) -> Reservation:
-        return Reservation(
-            facility=ARBITRARY_FACILITY,
-            time=TimeWindow(begin=datetime(year=2022, month=11, day=20), end=datetime(year=2022, month=11, day=21)))
-
-    @property
-    def _expected_suggestions(self) -> List[datetime]:
+    def _expected_suggestion_start_times(self) -> List[datetime]:
         return [
             datetime(year=2022, month=11, day=19),
             datetime(year=2022, month=11, day=22),
