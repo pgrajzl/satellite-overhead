519ff773946de001e93c9ef76852fde3453dc776
ellem95 <eloise.morris@colorado.edu>
2022-12-02 11:46:22 -0700

    Updated validator.py to get rid of timescale mistake

--- satellite_determination/validator.py
+++ satellite_determination/validator.py
@@ -4,22 +4,22 @@ import sys
 import numpy as nps
 from satellite_determination.dataclasses.reservation import Reservation
 from satellite_determination.dataclasses.overhead_window import OverheadWindow
-from satellite_determination.dataclasses.satellite import Satellite
+from satellite_determination.dataclasses.skyfield_satellite import SkyfieldSatelliteList
 from satellite_determination.validator.validator import Validator
 from satellite_determination.dataclasses.time_window import TimeWindow
-
+from skyfield.timelib import Timescale
+from skyfield.api import utc
 
 class ValidatorRhodesMill(Validator):
 
-    def overhead_list(self, list_of_satellites, reservation):
+    def overhead_list(self, list_of_satellites: SkyfieldSatelliteList, reservation: Reservation):
         ts = load.timescale()
         interferers = []
-        t0 = ts.utc(reservation.time_start)
-        t1 = ts.utc(reservation.time_end)
-        satellites = load.tle_file("./TLEData/")
-        coordinates = wgs84.latlon(reservation.point_coordinates.latitude, reservation.point_coordinates.longitude)
-        for sat in satellites:
-            t, events = sat.find_events(coordinates, t0, t1, altitude_degrees=reservation.Facility.angle_of_visibility_cone)
+        t0 = ts.utc(reservation.time.begin.replace(tzinfo=utc))
+        t1 = ts.utc(reservation.time.end.replace(tzinfo=utc))
+        coordinates = wgs84.latlon(reservation.facility.point_coordinates.latitude, reservation.facility.point_coordinates.longitude)
+        for sat in list_of_satellites.satellites:
+            t, events = sat.find_events(coordinates, t0, t1, altitude_degrees=reservation.facility.angle_of_visibility_cone)
             if events.size == 0:
                 continue
             else:
