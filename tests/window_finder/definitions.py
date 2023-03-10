511a6e532bad9bd2d4c51f68a243427b97a84f78
ellem95 <eloise.morris@colorado.edu>
2023-03-09 21:27:32 -0700

    Fix to window_finder definitions.py to add new azimuth field to ARBITRARY FACILITY so tests pass

--- tests/window_finder/definitions.py
+++ tests/window_finder/definitions.py
@@ -4,5 +4,6 @@ from satellite_determination.custom_dataclasses.facility import Facility
 ARBITRARY_FACILITY = Facility(
     angle_of_visibility_cone=1.,
     point_coordinates=Coordinates(latitude=1., longitude=2.),
-    name='name'
+    name='name',
+    azimuth=30
 )
