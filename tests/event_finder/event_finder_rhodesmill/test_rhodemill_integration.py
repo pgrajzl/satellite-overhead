4ba70abceb289460b035405e97f6ca2e9b83432f
nikojpapa <npapa1994@gmail.com>
2023-05-23 16:46:18 -0600

    test rhodesmill empty events

--- tests/event_finder/event_finder_rhodesmill/test_rhodemill_integration.py
+++ tests/event_finder/event_finder_rhodesmill/test_rhodemill_integration.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from functools import cached_property
 
+from numpy._typing import NDArray
 from skyfield.api import load, wgs84
 from skyfield.timelib import Time, Timescale
 
@@ -12,16 +13,43 @@ from satellite_determination.custom_dataclasses.satellite.tle_information import
 
 
 class TestRhodesmillIntegration:
+    _MINUTE_BEFORE_ENTERS = 34
+    _MINUTES_AFTER_ENTERS = _MINUTE_BEFORE_ENTERS + 1
+    _MINUTE_BEFORE_CULMINATES = 40
+    _MINUTE_AFTER_CULMINATES = _MINUTE_BEFORE_CULMINATES + 1
+    _MINUTE_BEFORE_LEAVES = 47
+
+    def test_events_found_on_window_that_encompasses_only_leaves(self):
+        assert self._get_events(minute_begin=self._MINUTE_AFTER_CULMINATES, minute_end=self._MINUTE_BEFORE_LEAVES).tolist() == []
+
+    def test_events_found_on_window_that_is_between_enter_and_culminates(self):
+        assert self._get_events(minute_begin=self._MINUTES_AFTER_ENTERS, minute_end=self._MINUTE_BEFORE_CULMINATES).tolist() == []
+
+    def test_events_found_on_window_that_encompasses_only_enters(self):
+        assert self._get_events(minute_begin=self._MINUTE_BEFORE_ENTERS, minute_end=self._MINUTE_BEFORE_CULMINATES).tolist() == []
+
+    def test_events_found_on_window_that_encompasses_only_culminates(self):
+        assert self._get_events(minute_begin=self._MINUTE_BEFORE_CULMINATES, minute_end=self._MINUTE_AFTER_CULMINATES).tolist() == [1]
+
+    def test_events_found_on_window_that_encompasses_culminates_and_leaves(self):
+        assert self._get_events(minute_begin=self._MINUTE_BEFORE_CULMINATES, minute_end=self._MINUTE_BEFORE_LEAVES).tolist() == [1, 2]
+
+    def test_events_found_on_window_that_encompasses_culminates_and_enters(self):
+        assert self._get_events(minute_begin=self._MINUTE_BEFORE_ENTERS, minute_end=self._MINUTE_AFTER_CULMINATES).tolist() == [0, 1]
+
     def test_events_found_on_window_that_encompasses_full_satellite_pass(self):
-        time_begin = self._datetime_to_rhodesmill_time(minute=0)
-        time_end = self._datetime_to_rhodesmill_time(minute=59)
+        assert self._get_events(minute_begin=self._MINUTE_BEFORE_ENTERS, minute_end=self._MINUTE_BEFORE_LEAVES).tolist() == [0, 1, 2]
+
+    def _get_events(self, minute_begin: int, minute_end: int) -> NDArray[int]:
+        time_begin = self._datetime_to_rhodesmill_time(minute=minute_begin)
+        time_end = self._datetime_to_rhodesmill_time(minute=minute_end)
         coordinates = wgs84.latlon(40.8178049, -121.4695413)
         rhodesmill_earthsat = self._satellite.to_rhodesmill()
         event_times, events = rhodesmill_earthsat.find_events(topos=coordinates,
                                                               t0=time_begin,
                                                               t1=time_end,
                                                               altitude_degrees=0)
-        assert len(events)
+        return events
 
 
     def _datetime_to_rhodesmill_time(self, minute: int) -> Time:
