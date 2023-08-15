cb793fa4db6bcac94d9ab6f36b1033c3ce04bc2b
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-08-15 13:11:07 -0600

    update get_antenna_direction_path to account for new json config antenna position option

--- satellite_determination/configuration_loader/configuration_loader_config_file.py
+++ satellite_determination/configuration_loader/configuration_loader_config_file.py
@@ -5,17 +5,18 @@ from satellite_determination.custom_dataclasses.frequency_range.support.get_freq
     GetFrequencyDataFromCsv
 from satellite_determination.utilities import get_frequencies_filepath, get_satellites_filepath
 from satellite_determination.path_finder.observation_path_finder import ObservationPathFinder
-from satellite_determination.config_file.support.config_file_base import ConfigFileBase
 from satellite_determination.custom_dataclasses.satellite.satellite import Satellite
 from satellite_determination.custom_dataclasses.reservation import Reservation
+from satellite_determination.custom_dataclasses.configuration import Configuration
+from satellite_determination.custom_dataclasses.position_time import PositionTime
 from satellite_determination.configuration_loader.configuration_loader import ConfigurationLoader
 
 class ConfigurationLoaderConfigFile(ConfigurationLoader):
-    def __init__(self, config_file: ConfigFileBase):
-        self.config_file = config_file
+    def __init__(self, config_file: Configuration):
+        self.config = config_file
 
     def get_reservation(self) -> Reservation:
-        return self.config_file.configuration.reservation
+        return self.config.reservation
 
     def get_satellite_list(self, tle_file, frequency_file) -> List[Satellite]:
         satellite_list = Satellite.from_tle_file(tlefilepath=tle_file)
@@ -27,11 +28,12 @@ class ConfigurationLoaderConfigFile(ConfigurationLoader):
         return satellite_list_with_frequencies
 
     def get_antenna_direction_path(self):
-        antenna_direction_path = [PositionTime(position=self.config_file.configuration.static_antenna_position,
-                                               time=self.config_file.configuration.reservation.time.begin)] \
-            if self.config_file.configuration.static_antenna_position \
-            else ObservationPathFinder(facility=self.config_file.configuration.reservation.facility,
-                                       observation_target=self.config_file.configuration.observation_target,
-                                       time_window=self.config_file.configuration.reservation.time).calculate_path()
-    
-        return antenna_direction_path
+        if self.config.antenna_position_times:
+            return self.config.antenna_position_times
+        elif self.config.static_antenna_position:
+            return [PositionTime(position=self.config.static_antenna_position,
+                                 time=self.config.reservation.time.begin)]
+        else:
+            return ObservationPathFinder(facility=self.config.reservation.facility,
+                                         observation_target=self.config.observation_target,
+                                         time_window=self.config.reservation.time).calculate_path()
