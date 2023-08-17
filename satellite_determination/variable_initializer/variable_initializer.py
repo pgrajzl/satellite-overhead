5dc0a3187ba5c3b902079ba262643680f637ffe3
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-08-16 19:35:36 -0600

    rename classes to be more clear. update base/subclass to use SatellitesLoader as a dependency injection.

--- satellite_determination/variable_initializer/variable_initializer.py
+++ satellite_determination/variable_initializer/variable_initializer.py
@@ -3,20 +3,20 @@ from typing import List
 
 from satellite_determination.custom_dataclasses.reservation import Reservation
 from satellite_determination.custom_dataclasses.satellite.satellite import Satellite
+from satellite_determination.custom_dataclasses.position_time import PositionTime
+from satellite_determination.satellites_loader.satellites_loader import SatellitesLoader
 
-class ConfigurationLoader(ABC):
-    @abstractmethod
-    def __init__(self):
-        pass
+class VariableInitializer(ABC):
+    def __init__(self, satellites_loader: SatellitesLoader):
+        self.satellites_loader = satellites_loader
 
     @abstractmethod
     def get_reservation(self) -> Reservation:
         pass
 
-    @abstractmethod
     def get_satellite_list(self) -> List[Satellite]:
-        pass
+        return self.satellites_loader.load_satellites()
 
     @abstractmethod
-    def get_antenna_direction_path(self):
+    def get_antenna_direction_path(self) -> List[PositionTime]:
         pass
--- satellite_determination/variable_initializer/variable_initializer_from_config.py
+++ satellite_determination/variable_initializer/variable_initializer_from_config.py
@@ -1,33 +1,23 @@
 from dataclasses import replace
 from typing import List
 
-from satellite_determination.custom_dataclasses.frequency_range.support.get_frequency_data_from_csv import \
-    GetFrequencyDataFromCsv
-from satellite_determination.utilities import get_frequencies_filepath, get_satellites_filepath
 from satellite_determination.path_finder.observation_path_finder import ObservationPathFinder
 from satellite_determination.custom_dataclasses.satellite.satellite import Satellite
 from satellite_determination.custom_dataclasses.reservation import Reservation
 from satellite_determination.custom_dataclasses.configuration import Configuration
 from satellite_determination.custom_dataclasses.position_time import PositionTime
-from satellite_determination.configuration_loader.configuration_loader import ConfigurationLoader
+from satellite_determination.variable_initializer.variable_initializer import VariableInitializer
+from satellite_determination.satellites_loader.satellites_loader import SatellitesLoader
 
-class ConfigurationLoaderConfigFile(ConfigurationLoader):
-    def __init__(self, config_file: Configuration):
-        self.config = config_file
+class VariableInitializerFromConfig(VariableInitializer):
+    def __init__(self, config: Configuration, satellites_loader: SatellitesLoader):
+        super().__init__(satellites_loader)
+        self.config = config
 
     def get_reservation(self) -> Reservation:
         return self.config.reservation
 
-    def get_satellite_list(self, tle_file, frequency_file) -> List[Satellite]:
-        satellite_list = Satellite.from_tle_file(tlefilepath=tle_file)
-        frequency_list = GetFrequencyDataFromCsv(filepath=frequency_file).get()
-
-        satellite_list_with_frequencies = [replace(satellite, frequency=frequency_list.get(satellite.tle_information.satellite_number, []))
-                                           for satellite in satellite_list]
-
-        return satellite_list_with_frequencies
-
-    def get_antenna_direction_path(self):
+    def get_antenna_direction_path(self) -> List[PositionTime]:
         if self.config.antenna_position_times:
             return self.config.antenna_position_times
         elif self.config.static_antenna_position:
