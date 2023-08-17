e4de9065d537b49fc79a3d2ee23eadc61d346d7c
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-08-16 19:35:58 -0600

    update tests to have stronger asserts

--- tests/variable_initializer/test_variable_initializer_from_config.py
+++ tests/variable_initializer/test_variable_initializer_from_config.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-import os
 import pytest
 import pytz
 
-from satellite_determination.configuration_loader.configuration_loader_config_file import ConfigurationLoaderConfigFile
+from satellite_determination.variable_initializer.variable_initializer_from_config import VariableInitializerFromConfig
+from satellite_determination.satellites_loader.satellites_loader import SatellitesLoader
+from satellite_determination.custom_dataclasses.satellite.satellite import Satellite
 from satellite_determination.custom_dataclasses.configuration import Configuration
 from satellite_determination.custom_dataclasses.reservation import Reservation
 from satellite_determination.custom_dataclasses.facility import Facility
@@ -15,9 +16,15 @@ from satellite_determination.custom_dataclasses.frequency_range.frequency_range 
 from satellite_determination.custom_dataclasses.position import Position
 from satellite_determination.custom_dataclasses.position_time import PositionTime
 
+class MockSatellitesLoader(SatellitesLoader):
+    def load_satellites(self):
+        return [
+            Satellite(name='Satellite1'),
+            Satellite(name='Satellite2'),
+        ]
 
-class TestConfigurationLoaderConfigFile:
-    @pytest.fixture(scope='class')
+class TestVariableInitializerFromConfig:
+    @property
     def common_config(self):
         return Configuration(
             reservation=Reservation(
@@ -39,24 +46,24 @@ class TestConfigurationLoaderConfigFile:
             static_antenna_position=None,
         )
 
-    @pytest.fixture
-    def observation_config(self, common_config):
-        config = common_config
+    @property
+    def observation_config(self):
+        config = self.common_config
         config.observation_target = ObservationTarget(
             declination='-38d6m50.8s',
             right_ascension='4h42m'
         )
         return config
 
-    @pytest.fixture
-    def static_config(self, common_config):
-        config = common_config
+    @property
+    def static_config(self):
+        config = self.common_config
         config.static_antenna_position = Position(altitude=.2, azimuth=.3)
         return config
 
-    @pytest.fixture
-    def antenna_position_config(self, common_config):
-        config = common_config
+    @property
+    def antenna_position_config(self):
+        config = self.common_config
         config.antenna_position_times = [
             PositionTime(
                 position=Position(altitude=.0, azimuth=.1),
@@ -69,30 +76,86 @@ class TestConfigurationLoaderConfigFile:
         ]
         return config
 
-    def test_get_reservation(self, observation_config):
-        config_loader = ConfigurationLoaderConfigFile(config_file=observation_config)
-        
-        reservation = config_loader.get_reservation()
-        assert isinstance(reservation, Reservation)
-        
-    def test_get_satellite_list(self, observation_config):
-        config_loader = ConfigurationLoaderConfigFile(config_file=observation_config)
-        
-        test_script_directory = os.path.dirname(os.path.abspath(__file__))
+    @property
+    def mock_satellites_loader(self):
+        return MockSatellitesLoader()
 
-        tle_file = os.path.join(test_script_directory, 'satellites.tle')
-        frequency_file = os.path.join(test_script_directory, 'satellite_frequencies.csv')
-
-        satellites = config_loader.get_satellite_list(tle_file=tle_file, frequency_file=frequency_file)
-        assert isinstance(satellites, list)
-        assert len(satellites) > 0
+    def test_get_reservation(self):
+        var_initializer = VariableInitializerFromConfig(
+                satellites_loader=self.mock_satellites_loader,
+                config=self.observation_config
+        )
         
+        reservation = var_initializer.get_reservation()
+        assert reservation == Reservation(
+            facility=Facility(
+                coordinates=Coordinates(latitude=40.8178049, longitude=-121.4695413),
+                beamwidth=3,
+                elevation=0,
+                height=100,
+                name='ARBITRARY_2'
+            ),
+            time=TimeWindow(
+                begin=datetime(year=2023, month=3, day=30, hour=10, tzinfo=pytz.utc),
+                end=datetime(year=2023, month=3, day=30, hour=15, tzinfo=pytz.utc)
+            ),
+            frequency=FrequencyRange(frequency=135, bandwidth=10, status=None)
+        )
 
-    @pytest.mark.parametrize('config', ['observation_config', 'static_config', 'antenna_position_config'])
-    def test_get_antenna_direction_path(self, config, request):
-        config = request.getfixturevalue(config)
-        config_loader = ConfigurationLoaderConfigFile(config_file=config)
+    def test_get_satellite_list(self):
+        var_initializer = VariableInitializerFromConfig(
+            satellites_loader=self.mock_satellites_loader,
+            config=self.common_config
+        )
+
+        satellite_list = var_initializer.get_satellite_list()
+        assert satellite_list[0].name == "Satellite1"
+        assert satellite_list[1].name == "Satellite2"
+
+    def test_get_antenna_direction_path_observation_config(self):
+        var_initializer = VariableInitializerFromConfig(
+                satellites_loader=self.mock_satellites_loader,
+                config=self.observation_config
+        )
+
+        antenna_direction_path = var_initializer.get_antenna_direction_path()
+        assert antenna_direction_path[0] == PositionTime(
+            position=Position(altitude=-63.291283818128676, azimuth=264.47226741909765),
+            time=datetime(year=2023, month=3, day=30, hour=10, minute=0, tzinfo=pytz.UTC)
+        )
+        assert antenna_direction_path[-1] == PositionTime(
+            position=Position(altitude=-58.827280171790655, azimuth=98.42630806438551),
+            time=datetime(year=2023, month=3, day=30, hour=15, minute=0, tzinfo=pytz.UTC)
+        )
+
+    def test_get_antenna_direction_path_static_config(self):
+        var_initializer = VariableInitializerFromConfig(
+                satellites_loader=self.mock_satellites_loader,
+                config=self.static_config
+        )
         
-        antenna_direction_path = config_loader.get_antenna_direction_path()
-        assert isinstance(antenna_direction_path, list)
-        assert len(antenna_direction_path) > 0
+        antenna_direction_path = var_initializer.get_antenna_direction_path()
+        assert antenna_direction_path == [
+            PositionTime(
+                position=Position(altitude=0.2, azimuth=0.3),
+                time=datetime(year=2023, month=3, day=30, hour=10, minute=0, tzinfo=pytz.UTC)
+            )
+        ]
+
+    def test_get_antenna_direction_path_antenna_position_config(self):
+        var_initializer = VariableInitializerFromConfig(
+                satellites_loader=self.mock_satellites_loader,
+                config=self.antenna_position_config
+        )
+        
+        antenna_direction_path = var_initializer.get_antenna_direction_path()
+        assert antenna_direction_path == [
+            PositionTime(
+                position=Position(altitude=0.0, azimuth=0.1),
+                time=datetime(year=2023, month=3, day=30, hour=10, minute=1, tzinfo=pytz.UTC)
+            ),
+            PositionTime(
+                position=Position(altitude=0.1, azimuth=0.2),
+                time=datetime(year=2023, month=3, day=30, hour=10, minute=2, tzinfo=pytz.UTC)
+            ),
+        ]
