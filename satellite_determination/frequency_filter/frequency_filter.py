32d513921bfd870479ad2ab5822213941328f292
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-09-06 15:33:50 -0600

    Make code more clear as to what satellites get added to the frequency filtered list

--- satellite_determination/frequency_filter/frequency_filter.py
+++ satellite_determination/frequency_filter/frequency_filter.py
@@ -20,11 +20,14 @@ class FrequencyFilter:
         frequency_filtered_satellite_list = []
 
         for sat in self._list_satellites:
-            if not sat.frequency or any(sf.frequency is None for sf in sat.frequency):
-                frequency_filtered_satellite_list.append(sat)
-                continue
+            has_missing_frequency = not sat.frequency or any(sf.frequency is None for sf in sat.frequency)
 
-            if any(sf.status != 'inactive' and self._observation_frequency.overlaps(sf) for sf in sat.frequency):
+            frequency_overlaps_target_frequency = any(
+                sf.status != 'inactive' and self._observation_frequency.overlaps(sf)
+                for sf in sat.frequency
+            )
+
+            if has_missing_frequency or frequency_overlaps_target_frequency:
                 frequency_filtered_satellite_list.append(sat)
 
         return frequency_filtered_satellite_list
