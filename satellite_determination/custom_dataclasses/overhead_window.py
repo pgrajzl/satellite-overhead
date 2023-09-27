ced0fa308758a5fd8d182ae576227945cd7a4074
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-09-27 15:24:39 -0600

    remove the TimeWindow attribute and add a List[PositionTimes]. The class remains backwards compatible because we can build a TimeWindow using the PositionTimes.

--- satellite_determination/custom_dataclasses/overhead_window.py
+++ satellite_determination/custom_dataclasses/overhead_window.py
@@ -1,16 +1,28 @@
 from dataclasses import dataclass
+from typing import List
 from satellite_determination.custom_dataclasses.satellite.satellite import Satellite
 from satellite_determination.custom_dataclasses.time_window import TimeWindow
+from satellite_determination.custom_dataclasses.position_time import PositionTime
 
 '''
 OverheadWindow class is designed to store the time windows that a given satellite is overhead and includes the Satellite object,
 as well as a TimeWindow object that contains the interference start and end times.
 
   + satellite:      the Satellite that is overhead during the time window.
-  + overhead_time:  a TimeWindow representing the time the satellite enters and exits view.
+  + positions:      a list of PositionTimes of the satellite while within the main beam
+  + overhead_time:  a property TimeWindow representing the time the satellite enters and exits view.
 '''
 
 @dataclass
 class OverheadWindow:
     satellite: Satellite
-    overhead_time: TimeWindow
+    positions: List[PositionTime]
+
+    @property
+    def overhead_time(self):
+        if self.positions:
+            begin = self.positions[0].time
+            end = self.positions[-1].time
+            return TimeWindow(begin=begin, end=end)
+        else:
+            return None
