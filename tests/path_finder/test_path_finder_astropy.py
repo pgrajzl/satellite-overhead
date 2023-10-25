b79f2a491867403b38a9ac6d3a080855bff3fb1b
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2023-10-25 16:10:13 -0600

    Add tests for ObservationPathFinder.

---
+++ tests/path_finder/test_path_finder_astropy.py
@@ -0,0 +1,6 @@
+from satellite_determination.path_finder.observation_path_finder_astropy import ObservationPathFinderAstropy
+from path_finder_base_test import PathFinderBaseTest
+
+
+class TestPathFinderAstropy(PathFinderBaseTest):
+    PathFinderClass = ObservationPathFinderAstropy
