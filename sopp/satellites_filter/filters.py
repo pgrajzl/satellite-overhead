5c30ba5f84cbd7c7d410a59e87731c5109d1892c
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2024-01-17 14:10:33 -0700

    Add type annotations.

--- sopp/satellites_filter/filters.py
+++ sopp/satellites_filter/filters.py
@@ -1,4 +1,10 @@
-def frequency_filter(observation_frequency):
+from typing import Callable
+
+from sopp.custom_dataclasses.frequency_range.frequency_range import FrequencyRange
+from sopp.custom_dataclasses.satellite.satellite import Satellite
+
+
+def frequency_filter(observation_frequency: FrequencyRange) -> Callable[[Satellite], bool]:
     """
     frequency_filter returns True if a satellite's downlink transmission frequency
     overlaps with the desired observation frequency. If there is no information
@@ -12,16 +18,16 @@ def frequency_filter(observation_frequency):
     - A lambda function that takes a Satellite object and returns True if the conditions
       for frequency filtering are met, False otherwise.
     """
-    return lambda sat: (
-        not sat.frequency or any(sf.frequency is None for sf in sat.frequency)
+    return lambda satellite: (
+        not satellite.frequency or any(sf.frequency is None for sf in satellite.frequency)
     ) or (
         any(
             sf.status != 'inactive' and observation_frequency.overlaps(sf)
-            for sf in sat.frequency
+            for sf in satellite.frequency
         )
     )
 
-def name_contains_filter(substring):
+def name_contains_filter(substring: str) -> Callable[[Satellite], bool]:
     """
     name_contains_filter returns a lambda function that checks if a given substring
     is present in the name of a Satellite.
@@ -33,9 +39,9 @@ def name_contains_filter(substring):
     - A lambda function that takes a Satellite object and returns True if the name
       contains the specified substring, False otherwise.
     """
-    return lambda sat: substring in sat.name
+    return lambda satellite: substring in satellite.name
 
-def name_does_not_contain_filter(substring):
+def name_does_not_contain_filter(substring: str) -> Callable[[Satellite], bool]:
     """
     name_does_not_contain_filter returns a lambda function that checks if a given substring
     is not present in the name of a Satellite.
@@ -47,9 +53,9 @@ def name_does_not_contain_filter(substring):
     - A lambda function that takes a Satellite object and returns True if the name
       does not contain the specified substring, False otherwise.
     """
-    return lambda sat: substring not in sat.name
+    return lambda satellite: substring not in satellite.name
 
-def name_is_filter(substring):
+def name_is_filter(substring: str) -> Callable[[Satellite], bool]:
     """
     name_is_filter returns a lambda function that checks if a given substring
     matches exactly the name of a Satellite.
@@ -61,9 +67,9 @@ def name_is_filter(substring):
     - A lambda function that takes a Satellite object and returns True if the name
       matches the specified substring exactly, False otherwise.
     """
-    return lambda sat: substring == sat.name
+    return lambda satellite: substring == satellite.name
 
-def is_leo_filter():
+def is_leo_filter() -> Callable[[Satellite], bool]:
     """
     is_leo_filter returns a lambda function to filter Low Earth Orbit (LEO) satellites based on their orbital period.
 
@@ -72,9 +78,9 @@ def is_leo_filter():
     Returns:
     - A lambda function that takes a Satellite object and returns True if it is in LEO, False otherwise.
     """
-    return lambda sat: sat.orbital_period <= 128.0
+    return lambda satellite: satellite.orbital_period <= 128.0
 
-def is_meo_filter():
+def is_meo_filter() -> Callable[[Satellite], bool]:
     """
     is_meo_filter returns a lambda function to filter Medium Earth Orbit (MEO) satellites based on their orbital period.
 
@@ -83,9 +89,9 @@ def is_meo_filter():
     Returns:
     - A lambda function that takes a Satellite object and returns True if it is in MEO, False otherwise.
     """
-    return lambda sat: sat.orbital_period > 128.0 and sat.orbital_period < 1430.0
+    return lambda satellite: satellite.orbital_period > 128.0 and satellite.orbital_period < 1430.0
 
-def is_geo_filter():
+def is_geo_filter() -> Callable[[Satellite], bool]:
     """
     is_geo_filter returns a lambda function to filter Geostationary Orbit (GEO) satellites based on their orbital period.
 
@@ -95,4 +101,4 @@ def is_geo_filter():
     Returns:
     - A lambda function that takes a Satellite object and returns True if it is in GEO, False otherwise.
     """
-    return lambda sat: abs(sat.orbital_period - 1436.0) <= 1.0
+    return lambda satellite: abs(satellite.orbital_period - 1436.0) <= 1.0
